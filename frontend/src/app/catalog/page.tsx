483adf1a31e507a6419a72d1c02144f2a614f28e
SemIDsss <andr.semidark@gmail.com>
2026-06-21 04:54:39 +0300

    f

--- frontend/src/app/catalog/page.tsx
+++ frontend/src/app/catalog/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+export const dynamic = 'force-dynamic';
 import React, { useState, useEffect, useRef, useCallback } from 'react';
 import Link from 'next/link';
 import { AddToCartButton } from '@/components/AddToCartButton';
